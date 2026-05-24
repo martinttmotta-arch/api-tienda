cb64feef2abf8945bfe9bfee9d2dfae6c5edadcf
martinttmotta-arch <martinttmotta@gmail.com>
2026-05-24 13:43:36 -0300

    Arreglo de admin

--- api/src/middleware/esAdmin.js
+++ api/src/middleware/esAdmin.js
@@ -1,4 +1,5 @@
 module.exports = (req, res, next) => {
+    console.log('Usuario:', req.usuario)
     if (req.usuario.rol !== 'admin') {
         return res.status(403).json({ error: 'Acceso denegado - solo administradores' })
     }
